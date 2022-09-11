d31e5cb5e569c6a553fd0dc678971944acc047b3
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-09-11 18:22:40 +0000

    http client setup

---
+++ shared/src/commonMain/kotlin/com/emma_ea/hellokmm/network/SpaceXApi.kt
@@ -0,0 +1,32 @@
+package com.emma_ea.hellokmm.network
+
+import com.emma_ea.hellokmm.entity.RocketLaunch
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.serialization.kotlinx.json.*
+import kotlinx.serialization.json.Json
+
+class SpaceXApi {
+
+    companion object {
+        private const val LAUNCHES_ENDPOINT = "https://api.spacexdata.com/v3/launches"
+    }
+
+    suspend fun getAllLaunches(): List<RocketLaunch> {
+        return httpClient.get(LAUNCHES_ENDPOINT).body()
+    }
+
+    private val httpClient = HttpClient {
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    prettyPrint = true
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
+    }
+}
