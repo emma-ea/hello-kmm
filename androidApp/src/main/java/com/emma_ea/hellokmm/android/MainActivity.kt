115d31bd8d9829a2d6de4fd1248027eb17492855
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-09-11 18:26:21 +0000

    activity layout spec, wire adapter to views

--- androidApp/src/main/java/com/emma_ea/hellokmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/emma_ea/hellokmm/android/MainActivity.kt
@@ -2,35 +2,64 @@ package com.emma_ea.hellokmm.android
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import com.emma_ea.hellokmm.Greeting
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
+import com.emma_ea.hellokmm.SpaceXSDK
+import com.emma_ea.hellokmm.sqldelight.DatabaseDriverFactory
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
-
-suspend fun greet(): String {
-    return Greeting().greeting()
-}
 
 class MainActivity : AppCompatActivity() {
 
     private val scope = MainScope()
 
+    private lateinit var launchesRecyclerView: RecyclerView
+    private lateinit var progressBarView: FrameLayout
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    private val launchesRvAdapter = LaunchesRvAdapter(listOf())
+
+    private val sdk = SpaceXSDK(DatabaseDriverFactory(this))
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tv: TextView = findViewById(R.id.text_view)
-        tv.text = getString(R.string.loader_text)
+        title = "SpaceX Launches"
 
+        launchesRecyclerView = findViewById(R.id.launchesListRv)
+        progressBarView = findViewById(R.id.progressBar)
+        swipeRefreshLayout = findViewById(R.id.swipeContainer)
+
+        launchesRecyclerView.adapter = launchesRvAdapter
+        launchesRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        swipeRefreshLayout.setOnRefreshListener {
+            swipeRefreshLayout.isRefreshing = false
+            displayLaunches(true)
+        }
+        displayLaunches(false)
+    }
+
+    private fun displayLaunches(needReload: Boolean) {
+        progressBarView.isVisible = true
         scope.launch {
             kotlin.runCatching {
-                greet()
+                sdk.getLaunches(needReload)
             }.onSuccess {
-                tv.text = it
+                launchesRvAdapter.launches = it
+                launchesRvAdapter.notifyDataSetChanged()
             }.onFailure {
-                tv.text = it.localizedMessage
+                Toast.makeText(this@MainActivity, it.localizedMessage, Toast.LENGTH_SHORT).show()
             }
+            progressBarView.isVisible = false
         }
     }
 
