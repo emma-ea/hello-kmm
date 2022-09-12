0d3e0c5789eef1093ad6653ec477e937c20dcf53
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-09-12 16:52:17 +0000

    compose dependencies

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -55,6 +55,10 @@ dependencies {
     implementation ("androidx.compose.ui:ui:$composeVersion")
     implementation ("androidx.lifecycle:lifecycle-runtime-ktx:2.5.1")
 
+    implementation("androidx.navigation:navigation-compose:2.5.2")
+
+    implementation("com.google.accompanist:accompanist-swiperefresh:0.25.1")
+
     debugImplementation ("androidx.compose.ui:ui-tooling:$composeVersion")
 
 }
