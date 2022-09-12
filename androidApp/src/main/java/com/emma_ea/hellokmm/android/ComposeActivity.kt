663e79ce9b0c6a8aad82884808b9f89125204877
emmanuel awuah <emmanuelawuah.dev@gmail.com>
2022-09-12 16:53:01 +0000

    separate home view from main compose entry

--- androidApp/src/main/java/com/emma_ea/hellokmm/android/ComposeActivity.kt
+++ androidApp/src/main/java/com/emma_ea/hellokmm/android/ComposeActivity.kt
@@ -3,8 +3,16 @@ package com.emma_ea.hellokmm.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.emma_ea.hellokmm.android.screens.Home
+import com.emma_ea.hellokmm.android.screens.Screens
+import com.emma_ea.hellokmm.android.viewmodel.HomeViewModel
 
 
 class ComposeActivity : ComponentActivity() {
@@ -14,14 +22,24 @@ class ComposeActivity : ComponentActivity() {
 
         setContent {
             HelloKMMTheme {
-               Greetings()
+               Surface(color = MaterialTheme.colors.background) {
+                   val navController = rememberNavController()
+                   NavHost(
+                       navController = navController,
+                       startDestination = Screens.Home.route)
+                   {
+                        composable(route = Screens.Home.route) {
+                            Home(navController = navController, viewModel = HomeViewModel(baseContext))
+                        }
+                   }
+               }
             }
         }
     }
 
     @Composable
     fun Greetings() {
-        Text(text = "Hello, KMM")
+        Text(text = "Hello, Migrating to KMM")
     }
 
 }
--- androidApp/src/main/java/com/emma_ea/hellokmm/android/Previews.kt
+++ androidApp/src/main/java/com/emma_ea/hellokmm/android/Previews.kt
@@ -3,6 +3,10 @@ package com.emma_ea.hellokmm.android
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.emma_ea.hellokmm.android.components.LaunchInfo
+import com.emma_ea.hellokmm.entity.Links
+import com.emma_ea.hellokmm.entity.Rocket
+import com.emma_ea.hellokmm.entity.RocketLaunch
 
 @Preview("greetings")
 @Composable
@@ -13,4 +17,21 @@ fun Previews() {
 @Composable
 fun Greetings() {
     Text(text = "Hello, Migrating to Compose")
+}
+
+@Preview("Launch Card")
+@Composable
+fun LaunchCard() {
+    LaunchInfo(launch =
+        RocketLaunch(
+            1,
+            "mission",
+            2014,
+            "date",
+            Rocket("id", "name", "type"),
+            "details",
+            true,
+            Links("missionPatchUrl", "articleUrl")
+        )
+    )
 }
